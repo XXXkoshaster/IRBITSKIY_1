38593955a6f783898b9ac6bc093c22636bf9cbc3
Yanovich <vorobyovgleb14@gmail.com>
2024-10-04 21:00:51 +0300

    Added x from command prompt

--- LAB_1/task_5/task_5.c
+++ LAB_1/task_5/task_5.c
@@ -1,14 +1,16 @@
 #include "functions_5.h"
 
 int main(int argc, char **argv) {
-    double x = 1.0;
     char* epsilon = argv[1];
-    double epsilon_double = atof(epsilon);
+    char* x = argv[2];
 
-    printf("First expression: %f\n", first_expression(x, epsilon_double));
-    printf("Second expression: %f\n", second_expression(x, epsilon_double));
-    printf("Third expression: %f\n", third_expression(x, epsilon_double));
-    printf("Fourth expression: %f\n", fourth_expression(x, epsilon_double));
+    double epsilon_double = atof(epsilon);
+    double x_double = atof(x);
+    
+    printf("First expression: %f\n", first_expression(x_double, epsilon_double));
+    printf("Second expression: %f\n", second_expression(x_double, epsilon_double));
+    printf("Third expression: %f\n", third_expression(x_double, epsilon_double));
+    printf("Fourth expression: %f\n", fourth_expression(x_double, epsilon_double));
 
     return 0;
 }
