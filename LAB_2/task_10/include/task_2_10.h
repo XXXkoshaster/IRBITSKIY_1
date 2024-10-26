0adb30a18a54a26d57048b72ab835f391e1e5707
Yan0vich <vorobyovgleb14@gmail.com>
2024-10-26 14:54:00 +0300

    ...

--- LAB_2/task_10/include/task_2_10.h
+++ LAB_2/task_10/include/task_2_10.h
@@ -4,7 +4,7 @@
 #include "utils.h"
 
 enum ERRORS coef_calculation(double x, double *coefs, int count, double *res);
-enum ERRORS calc_differ(double *coefs, int count, int iter);
+enum ERRORS calculation_differ(double *coefs, int count, int iter);
 enum ERRORS decomposition_of_a_polynomial(int count, double eps, double x, double **result, ...);
 void print_coefficients(double *coefs, int count);
 
--- LAB_2/task_10/src/utils_2_10.c
+++ LAB_2/task_10/src/utils_2_10.c
@@ -3,10 +3,10 @@
 double factorial(int n)
 {
     double result = 1.0;
-    for (int i = 2; i <= n; i++)
-    {
+    for (int i = 2; i <= n; i++) {
         result *= i;
     }
+    
     return result;
 }
 
