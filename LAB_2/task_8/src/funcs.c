e92cccb92714a5f99727815faf66f04bde94c2d1
Yan0vich <vorobyovgleb14@gmail.com>
2024-10-26 14:46:59 +0300

    ...

--- LAB_2/task_8/src/funcs.c
+++ LAB_2/task_8/src/funcs.c
@@ -1,74 +1,63 @@
 #include "../include/task_2_8.h"
 
-// Функция для суммирования двух чисел, представленных в виде строк в заданной системе счисления
 enum ERRORS sum_in_base(char** result, const char* num_1, const char* num_2, int base)
 {
-    // Вычисление длины входных строк
     int length_1 = strlen(num_1);
     int length_2 = strlen(num_2);
 
-    // Определение максимальной длины
     int max_length = length_1 > length_2 ? length_1 : length_2;
 
-    // Выделение памяти для строки результата
     *result = (char*)malloc(max_length + 2);
     if (*result == NULL) {
         return INVALID_MEMORY;
     }
 
-    // Инициализация строки результата
     (*result)[max_length + 1] = '\0';
 
-    int reminder = 0; // Переменная для хранения переноса
-    int i = length_1 - 1; // Индекс для первой строки
-    int j = length_2 - 1; // Индекс для второй строки
-    int k = max_length; // Индекс для строки результата
+    int reminder = 0;
+    int i = length_1 - 1;
+    int j = length_2 - 1;
+    int k = max_length; 
 
-    // Итерация по цифрам строк справа налево
     while(k >= 0) {
-        int sum = reminder; // Инициализация суммы переносом
+        int sum = reminder; 
 
-        // Суммирование цифр первой строки
         if (i >= 0) {
             int r_part_1 = 0;
             enum ERRORS status = char_to_int(&r_part_1, num_1[i--], base);
 
             if (status != DONE) {
-                free(*result); // Освобождение памяти в случае ошибки
+                free(*result); 
                 return status;
             }
 
             sum += r_part_1;
         }
 
-        // Суммирование цифр второй строки
         if (j >= 0) {
             int r_part_2 = 0;
             enum ERRORS status = char_to_int(&r_part_2, num_2[j--], base);
 
             if (status != DONE) {
-                free(*result); // Освобождение памяти в случае ошибки
+                free(*result); 
                 return status;
             }
             
             sum += r_part_2;
         }
 
-        // Преобразование суммы в символ и сохранение в строке результата
         (*result)[k--] = int_to_char(sum % base);
-        reminder = sum / base; // Обновление переноса
+        reminder = sum / base;
     }
 
-    // Обработка оставшегося переноса
     if(reminder > 0) {
         (*result)[0] = int_to_char(reminder);
         return DONE;
     }
 
-    // Удаление ведущих нулей из результата
     if ((*result)[0] == '0') {
         char *new_result = strdup(*result + 1);
-        free(*result); // Освобождение старой строки результата
+        free(*result); 
         *result = new_result;
         return DONE;
     }
@@ -76,16 +65,13 @@ enum ERRORS sum_in_base(char** result, const char* num_1, const char* num_2, int
     return DONE;    
 }
 
-// Функция для суммирования нескольких чисел, представленных в виде строк в заданной системе счисления
 enum ERRORS getting_sums(int base, int count_nums, ...)
 {
-    // Проверка корректности системы счисления
     if (base < 2 || base > 36) {
         printf("Invalid base\n");
         return INVALID_INPUT;
     }
 
-    // Проверка корректности количества чисел
     if (count_nums <= 0) {
         printf("Invalid count of numbers\n");
         return INVALID_INPUT;
@@ -94,9 +80,8 @@ enum ERRORS getting_sums(int base, int count_nums, ...)
     va_list args;
     va_start(args, count_nums);
 
-    char* result = strdup("0"); // Инициализация результата как "0"
+    char* result = strdup("0");
 
-    // Итерация по входным числам
     for (int i = 0; i < count_nums; i++) {
         const char* num = va_arg(args, const char *);
         
@@ -108,7 +93,6 @@ enum ERRORS getting_sums(int base, int count_nums, ...)
             return NULL_PTR;
         }
 
-        // Суммирование текущего числа с результатом
         enum ERRORS status = sum_in_base(&new_result, result, num, base);
         
         if (status != DONE) {
@@ -118,7 +102,7 @@ enum ERRORS getting_sums(int base, int count_nums, ...)
             return status;
         }
 
-        free(result); // Освобождение старого результата
+        free(result); 
         result = new_result;
     }
 
@@ -126,19 +110,18 @@ enum ERRORS getting_sums(int base, int count_nums, ...)
 
     char* start = result;
 
-    // Удаление ведущих нулей из результата
     while (*result == '0' && *(result + 1) != '\0')
         result++;
 
     if (start != result) {
         char* new_result = strdup(result);
-        free(start); // Освобождение старого результата
+        free(start); 
         result = new_result;
     }
     
     printf("Result: %s\n", result);
 
-    free(result); // Освобождение памяти результата
-    
+    free(result); 
+
     return DONE;
 }
